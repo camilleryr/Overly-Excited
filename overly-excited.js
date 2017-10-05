f3e6422e2cddebfe455846dee5da32d989dc7e05
Christopher Miller <camiller.yr@gmail.com>
2017-10-05 11:25:57 -0500

    Changed to a strict equal

--- overly-excited.js
+++ overly-excited.js
@@ -2,24 +2,9 @@
 
 let sentence = ["The", "walrus", "danced", "through", "the", "trees", "in", "the", "light", "of", "the", "moon"];
 
-// let betterSentence = ["A", "man", "jumped", "then", "he", "fell", "went", "to", "hell", "then", "he", "died"]
-/*
-    The addExcitement function should be an impure function, and accept 
-    the array as the sole argument. It should iterate over the array 
-    and output the words to the browser console.
-*/
-// let addExcitement = (theWordArray, punctuation) => {
-//     for (index = 0; index <= theWordArray.length; index++){
-//         if (index > 0 && index % 3 == 0){
-//             theWordArray[index-1] += punctuation.repeat(index/3);
-//         }
-//         console.log(theWordArray.slice(0,index).join(" "));
-//     }
-// }
-
 let addExcitement = (theWordArray, punctuation) => {
     for (index = 0; index <= theWordArray.length; index++){
-        if (index % 3 == 0 && index != 0) {
+        if (index % 3 === 0 && index != 0) {
             theWordArray[index-1] += punctuation.repeat(index/3);
         }
         console.log(theWordArray.slice(0,index).join(" "));
@@ -28,4 +13,3 @@ let addExcitement = (theWordArray, punctuation) => {
 
 // Invoke the function and pass in the array
 addExcitement(sentence, "?");
-//addExcitement(betterSentence, " &");
